15185d302a3fe1634672a9a686a0533c28bead26
Aarya Chauhan <aaryac2004@gmail.com>
2025-12-27 22:20:00 +0530

    Time: 0 ms (100%), Space: 11.1 MB (84.49%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   void reverse(ListNode* head, int times){
+         ListNode* curr = head;
+         ListNode* prev = NULL;
+         while(times--&& curr){
+            ListNode* nex = curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nex;
+         }
+   }
+    ListNode* swapPairs(ListNode* head) {
+        if(head==NULL)return head;
+        ListNode* left = head;
+        ListNode*prevleft = NULL;
+        ListNode* res = NULL;
+        int size = 2;
+        while(true){
+             ListNode* right= left;
+            for(int i = 0; i<(size-1);i++){
+                if(right==NULL)break;
+                 right=right->next;
+            }
+            if(right){//left right muiil chuka hain
+            ListNode* nextleft = right->next;
+            reverse(left,size);
+            if(prevleft)
+            prevleft->next=right;
+            left->next = nextleft;
+            prevleft=left;
+            if(res==NULL)res=right;
+            left=nextleft;}
+            else{
+                if(prevleft)
+                prevleft->next=left;
+                if(res==NULL)
+                res=left;
+                break;
+            }
+            }
+            return res;
+        }
+    };
