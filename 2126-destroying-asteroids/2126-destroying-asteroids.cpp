eb312dde24a7bb7590895a57052d23e719117f6b
Aarya Chauhan <aaryac2004@gmail.com>
2026-05-31 21:42:14 +0530

    Time: 37 ms (31.97%), Space: 106.9 MB (16.2%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+       long long currentmass = mass;
+    for(int i = 0 ;i <asteroids.size();i++){
+        if(currentmass < asteroids[i]){
+            return false;
+        }
+        currentmass = currentmass+asteroids[i];
+    }
+    return true;
+    }
+};
