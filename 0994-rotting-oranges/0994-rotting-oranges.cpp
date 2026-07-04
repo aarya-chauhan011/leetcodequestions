4cb1b5b66c4316cfd875f4813705e8186a4c3930
Aarya Chauhan <aaryac2004@gmail.com>
2026-07-04 20:25:34 +0530

    Time: 3 ms (27.14%), Space: 16.8 MB (69.96%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,60 +1,55 @@
 class Solution {
 public:
+    int x[4] = {-1,1,0,0};   
+    int y[4] = { 0,0,-1,1};
+
+    bool valid(int i, int j, int n, int m){
+        if(i<0 || i>=n || j<0 || j>=m){
+            return false;   
+        }
+        return true;        
+    }
+
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
+        int n = grid.size();      
         int m = grid[0].size();
-        queue<pair<int,int>> Q;
-        vector<vector<int>> status(n, vector<int>(m, -1));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
-                    Q.push({i,j});
-                    status[i][j] = 0;
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        int time = 0;
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0; j<m ; j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                    grid[i][j]=-2;
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
         }
-        while(!Q.empty()){
-            auto p = Q.front();
-            Q.pop();
-            int i = p.first;
-            int j = p.second;
-            // right
-            if(j+1 < m && grid[i][j+1] == 1){
-                grid[i][j+1] = 2;
-                status[i][j+1] = status[i][j] + 1;
-                Q.push({i, j+1});
-            }
-
-            // left
-            if(j-1 >= 0 && grid[i][j-1] == 1){
-                grid[i][j-1] = 2;
-                status[i][j-1] = status[i][j] + 1;
-                Q.push({i, j-1});
-            }
-
-            // up
-            if(i-1 >= 0 && grid[i-1][j] == 1){
-                grid[i-1][j] = 2;
-                status[i-1][j] = status[i][j] + 1;
-                Q.push({i-1, j});
-            }
-
-            // down
-            if(i+1 < n && grid[i+1][j] == 1){
-                grid[i+1][j] = 2;
-                status[i+1][j] = status[i][j] + 1;
-                Q.push({i+1, j});
+        while(!q.empty() && fresh > 0){
+            time++;
+            int s = q.size();
+            while(s--){
+                pair<int,int> p = q.front();
+                q.pop();
+                int r = p.first;
+                int c = p.second;
+                for(int k = 0; k<4; k++){
+                    int row = r+x[k];
+                    int col = c+y[k];
+                    if(valid(row,col,n,m) && grid[row][col]==1){
+                        q.push({row,col});
+                        grid[row][col]=-2;
+                        fresh--;
+                    }
+                }
             }
         }
-        int ans = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 1)
-                    return -1;
-
-                ans = max(ans, status[i][j]);
-            }
+        if(fresh>0){
+            return -1;
         }
-        return ans;
+        return time;
     }
 };
